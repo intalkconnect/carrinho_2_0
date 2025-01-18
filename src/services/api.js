bf7c5ee3cdd9e9c59fc7f3eba02f39601fecfbe3
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-17 22:18:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 export const consultarAPI = async (id) => {
-    // Verifica se o ID não foi fornecido
-    if (!id) {
-        return []; // Retorna um array vazio ou qualquer outro valor adequado
+    // Verifica se o ID não foi fornecido ou está vazio
+    if (!id || id.trim() === "") {
+        console.error('ID é obrigatório');
+        return []; // Retorna um array vazio sem fazer a requisição
     }
 
     const url = `https://endpoints-checkout.rzyewu.easypanel.host/orcamento?id=${id}`;
@@ -15,9 +16,11 @@ export const consultarAPI = async (id) => {
         const data = await response.json();
         return data;
     } catch (error) {
+        console.error(error); // Exibe o erro no console, se houver
         return [];
     }
 };
+
 
 export const consultarCEP = async (cep) => {
     const url = `https://endpoints-checkout.rzyewu.easypanel.host/frete?cep=${cep}`;
