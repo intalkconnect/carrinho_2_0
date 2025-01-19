49112a89b8f3aaa805e202758e8e8116c5b78e8f
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-19 19:17:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Checkout from './components/Checkout';
 import CrudPage from './components/CrudPage';
-import { Analytics } from "@vercel/analytics/react";
 
 const router = createBrowserRouter(
   [
-    {
-      path: "/",
-      element: <Checkout />,
-    },
-    {
-      path: "/crud",
-      element: <CrudPage />,
-    },
-    {
-      path: "/:id",
-      element: <Checkout />,
-    },
-  ],
-  {
-    future: {
-      v7_startTransition: true,
-    },
-  }
+const router = createBrowserRouter(
 );
 
 const App = () => {
-  return (
-    <>
-      <RouterProvider router={router} />
-      <Analytics />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
