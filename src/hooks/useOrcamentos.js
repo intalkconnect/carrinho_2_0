f724785f1f99490e8ba552e2ce33da68e1ca0e43
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-22 14:36:01 -0300

    Update useOrcamentos.js

--- src/hooks/useOrcamentos.js
+++ src/hooks/useOrcamentos.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios from "axios";
 import { consultarAPI, sendClickCta } from '../services/api'; // Certifique-se de ajustar o caminho
 
 const useOrcamentos = () => {
@@ -17,11 +16,13 @@ const useOrcamentos = () => {
                 setStatus(data.status || ''); // Define o status retornado pela API
                 updateTotalValue(data.orcamento || []);
                 
-               const paciente = orcamentos[0]?.orc_paciente;
-               const pacienteAjustado = paciente.replace('B', '');
-               const identity = `${pacienteAjustado}@wa.gw.msging.net`;
+                if (status === 'pending'){
+                const paciente = orcamentos[0]?.orc_paciente;
+                const pacienteAjustado = paciente.replace('B', '');
+                const identity = `${pacienteAjustado}@wa.gw.msging.net`;
                 
-               const click = await sendClickCta(identity);
+                const click = await sendClickCta(identity);
+                }
             } catch (error) {
                 return [];
             }
