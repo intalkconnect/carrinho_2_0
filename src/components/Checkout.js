7c1b90f0035d1c00824888f2304a970e53f97396
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-01-21 23:34:23 -0300

    Update Checkout.js

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -194,6 +194,7 @@ const Checkout = () => {
             produtos: produtosValidos,
             frete: parseFloat(formData.frete),
             total: produtosValidos.reduce((sum, produto) => sum + produto.orc_qt_potes * produto.orc_valor_liquido, 0) + parseFloat(formData.frete),
+            identity: orcamentos[0].orc_paciente
         };
 
         // Envia o payload para a API
