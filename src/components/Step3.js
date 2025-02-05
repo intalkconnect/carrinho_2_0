a53b4f57e4265ad890a9d2fa8d5aa51ed5547c7f
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-02-05 10:43:07 -0300

    Update Step3.js

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -55,6 +55,40 @@ const Step3 = ({ finalizeCheckout, totalValue, formData }) => {
     const ASaasToken = '$aact_MzkwODA2MWY2OGM3MWRlMDU2NWM3MzJlNzZmNGZhZGY6OjljNjY3NzAzLWVlMzMtNDNlZS1iMDc4LTBhNzc1YjNmM2EwMDo6JGFhY2hfNDRjYzJlNDAtMmM4MC00MmJjLWEwN2MtOWJlNDE5MmEwYTQ5';
     const baseURL = 'https://endpoints-checkout.rzyewu.easypanel.host';
 
+    const detectCardBrand = (number) => {
+        const cleanNumber = number.replace(/\D/g, '');
+        if (!cleanNumber) return '';
+
+        const patterns = {
+            visa: /^4/,
+            mastercard: /^5[1-5]/,
+            amex: /^3[47]/,
+            elo: /^(401178|401179|431274|438935|451416|457393|457631|457632|504175|627780|636297|636368|636369|(506699|5067[0-6]\d|50677[0-8])|(50900\d|5090[1-9]\d|509[1-9]\d{2})|65003[1-3]|(65003[5-9]|65004\d|65005[0-1])|(65040[5-9]|6504[1-3]\d)|(65048[5-9]|65049\d|6505[0-2]\d|65053[0-8])|(65054[1-9]|6505[5-8]\d|65059[0-8])|(65070\d|65071[0-8])|65072[0-7]|(6509[0-9])|(6516[5-7])|(6550[0-5])|655021)/,
+            hipercard: /^(384100|384140|384160|606282|637095|637568|60(?!11))/,
+            diners: /^3(?:0[0-5]|[68][0-9])[0-9]/
+        };
+
+        for (const [brand, pattern] of Object.entries(patterns)) {
+            if (pattern.test(cleanNumber)) {
+                return brand;
+            }
+        }
+
+        return '';
+    };
+
+    const getCardBrandIcon = (brand) => {
+        const brandIcons = {
+            visa: '💳 Visa',
+            mastercard: '💳 Mastercard',
+            amex: '💳 American Express',
+            elo: '💳 Elo',
+            hipercard: '💳 Hipercard',
+            diners: '💳 Diners Club'
+        };
+        return brandIcons[brand] || '';
+    };
+
     // Funções auxiliares
     const fetchCustomer = async () => {
         try {
