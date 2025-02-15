b35fd41340fc92105dad1b3b7dfbd90d64e0bbdb
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-02-15 13:44:43 -0300

    Delete src directory

--- src/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-    // Modificar a URL do WebSocket para localhost ou a URL desejada
-    if (env === 'development') {
-      config.devServer.client.webSocketURL = 'wss://endpoints-checkout.rzyewu.easypanel.host/';
-    }
-    return config;
-  };
-  
